0ee706694194c4ba18f42e29e4ad7757c9be7284
Hitesh <Hitesh@192.168.5.54>
2019-11-26 23:18:00 -0800

    demo push

---
+++ MyMavenPractice/src/test/java/com/practice/Testing2.java
@@ -0,0 +1,16 @@
+package com.practice;
+
+import org.testng.annotations.Test;
+
+public class Testing2 {
+	
+	 @Test
+	  public void testing5() {
+		  System.out.println("testing5");
+	  }
+	  @Test
+	  public void testing4() {
+		  System.out.println("testing4");
+	  }
+
+}
